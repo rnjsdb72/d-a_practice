a48d92814309eac3594fc721900d65a2aa7a36ac
jeongha2 <gimjeongha2@gmail.com>
2022-07-05 15:00:53 +0900

    Resnetblock 추가했습니다.

--- final_practice1/model.py
+++ final_practice1/model.py
@@ -117,6 +117,21 @@ class ResNet(nn.Module):
     
     def _forward_impl(self, x):
         # 여기 구현
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
         
     def forward(self, x):
         return self._forward_impl(x)
